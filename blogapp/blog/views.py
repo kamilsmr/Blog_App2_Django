bc9e0c52e1406e5983b15c6274adc6779fcfa679
kamilsmr <kamils6035@gmail.com>
2022-08-31 21:05:56 +0300

    urls

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,3 +1,16 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("home page")
+
+
+def blogs(request):
+    return HttpResponse("blog page")
+
+
+
+def blog_details(request):
+    return HttpResponse("blog details")

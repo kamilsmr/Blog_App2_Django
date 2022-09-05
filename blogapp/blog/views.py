be571288b31a433413c2c8025a0b677b6a1e90d0
kamilsmr <kamils6035@gmail.com>
2022-09-05 22:46:24 +0300

    dinamik

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -3,8 +3,47 @@ from django.shortcuts import render
 
 # Create your views here.
 
+data = {
+    "blogs": [
+        {
+            "id":1,
+            "title":"komple web geliştirme",
+            "image":"1.jpg",
+            "is_active":True,
+            "description":"Çok iyi bir kurs",
+        },
+        {
+            "id":2,
+            "title":"Python Kursu",
+            "image":"2.jpg",
+            "is_active":True,
+            "description":"Çok iyi bir kurs",
+        },
+        {
+            "id":3,
+            "title":"Django Kursu",
+            "image":"3.jpg",
+            "is_active":True,
+            "description":"Çok iyi bir kurs",
+        },
+        {
+            "id":4,
+            "title":"Java",
+            "image":"4.jpg",
+            "is_active":False,
+            "description":"Çok iyi bir kurs",
+        },
+
+    ]
+}
+
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
+
+
 
 
 

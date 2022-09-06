d4e475a2a88488c75ea17423fe42a5b8cdbd8b0c
kamilsmr <kamils6035@gmail.com>
2022-09-07 00:30:02 +0300

    upload

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.utils.text import slugify
 # Create your models here.
 
+
+# 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="blogs")
     description = models.TextField()
     is_active = models.BooleanField()
     is_home = models.BooleanField()

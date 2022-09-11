1637ac4147cd69fb7541e29ef976c2c3a28f4124
kamilsmr <kamils6035@gmail.com>
2022-09-12 00:05:00 +0300

    uptade

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=150)
-    slug = models.SlugField(null=True, blank=True, unique=True, db_index=True, editable=False)
+    slug = models.SlugField(null=False,blank=True,unique=True,db_index=True,editable=False)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -13,16 +13,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="blogs")
     description = RichTextField()
     is_active = models.BooleanField(default=False)
     is_home = models.BooleanField(default=False)
-    slug = models.SlugField(null=True, blank=True, unique=True, db_index=True, editable=False)
-    categories = models.ManyToManyField(Category)
-
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -32,5 +30,4 @@ class Blog(models.Model):
         super().save(*args, **kwargs)
 
 
-    
 
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -2,52 +2,30 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from blog.models import Blog, Category
 
-
-# Create your views here.
-
-
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True,is_home=True),
-        "categories":Category.objects.all()
+        "blogs": Blog.objects.filter(is_active=True, is_home=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/index.html", context)
-
-
-
-
-
 
 def blogs(request):
     context = {
         "blogs": Blog.objects.filter(is_active=True),
-        "categories":Category.objects.all()
+        "categories": Category.objects.all()
     }
     return render(request, "blog/blogs.html", context)
 
-
-
 def blog_details(request, slug):
-    # blogs = data["blogs"]
-    # selectedBlog = None
-
-    # for blog in blogs:
-    #     if blog["id"] == id:
-    #         selectedBlog = blog
     blog = Blog.objects.get(slug=slug)
-
-
     return render(request, "blog/blog-details.html", {
         "blog": blog
     })
 
-
-
 def blogs_by_category(request, slug):
     context = {
-        "blogs": Blog.objects.filter(is_active=True, category__slug=slug),
-        "categories":Category.objects.all(),
-        "selected_category":slug
-
+        "blogs": Category.objects.get(slug=slug).blog_set.filter(is_active=True),
+        "categories": Category.objects.all(),
+        "selected_category": slug
     }
     return render(request, "blog/blogs.html", context)

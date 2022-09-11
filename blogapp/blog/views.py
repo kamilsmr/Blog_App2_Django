2d6d024b01d61e160ec603c09aed6f97db20129e
kamilsmr <kamils6035@gmail.com>
2022-09-11 19:49:15 +0300

    category

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,43 +1,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from blog.models import Blog
+from blogapp.blog.models import Category
 
 # Create your views here.
 
-data = {
-    "blogs": [
-        {
-            "id":1,
-            "title":"komple web geliştirme",
-            "image":"1.jpg",
-            "is_active":True,
-            "is_home":False,
-            "description":"Çok iyi bir kurs",
-        },
-        {
-            "id":2,
-            "title":"Python Kursu",
-            "image":"2.jpg",
-            "is_active":True,
-            "is_home":True,
-            "description":"Çok iyi bir kurs",
-        },
-        {
-            "id":3,
-            "title":"Django Kursu",
-            "image":"3.jpg",
-            "is_active":False,
-            "is_home":True,
-            "description":"Çok iyi bir kurs",
-        },
-        
-
-    ]
-}
 
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True,is_home=True)
+        "blogs": Blog.objects.filter(is_active=True,is_home=True),
+        "categories":Category.objects.all()
     }
     return render(request, "blog/index.html", context)
 
@@ -48,7 +20,8 @@ def index(request):
 
 def blogs(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True)
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories":Category.objects.all()
     }
     return render(request, "blog/blogs.html", context)
 
